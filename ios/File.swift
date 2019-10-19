7e318f3288d75934780df572ed73dcc565159253
hyeon(macbook pro) <antaehyeon@live.co.kr>
2019-10-19 18:19:25 +0900

    Feat: lottie 를 이용한 애니메이션 기능 추가
    - react-native-lottie 설치
    - 예시 json 추가 (intro, loading)

---
+++ ios/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  ReactNativeTech
+//
+//  Created by 안태현 on 2019/10/19.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+import Foundation
--- src/Splash.js
+++ src/Splash.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { View, Text } from "react-native";
 import { Actions } from "react-native-router-flux";
+import LottieView from "lottie-react-native";
 
 const Splash = () => {
   useEffect(() => {
@@ -11,7 +12,7 @@ const Splash = () => {
 
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Splash Screen</Text>
+      <LottieView source={require("./assets/animation/10431-intro.json")} autoPlay loop></LottieView>
     </View>
   );
 };
